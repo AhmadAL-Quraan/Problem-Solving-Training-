bea1485d78ab51dfe4cfe293dc5cbd2fc2bd8828
Ahmad AL-Quraan <ahmadquraan.git@gmail.com>
2024-09-27 00:36:02 +0300

    Add files via upload

---
+++ Problem Solving and CP/DataStructers and Algorithm/Dynammic Prgoramming/K-tree 1600/solve.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) v.begin(), v.end()
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+int dp[101][101];
+void solve() {
+
+}
+int main() {
+	fast_io;
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("outputt.txt", "w", stdout);
+#endif
+
+	// int T;
+	// cin >> T;
+	// while (T--)
+	solve();
+
+}
